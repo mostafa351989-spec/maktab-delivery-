19af99f39dd61f53874a21f68938b3f31c00dff5
اسمك على GitHub <>
2026-05-06 00:48:08 +0300

    rebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/maktab-Public-/',
+  plugins: [react()],
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/maktab-Public-/',
   plugins: [react()],
 })
